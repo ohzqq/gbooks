40847dc86a171d6294554b25ccd53e8e859fd687
ohzqq <iamchurkey@gmail.com>
2024-04-27 14:03:02 -0400

    move

--- cmd/cmd/searchCmd.go
+++ cmd/cmd/searchCmd.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"github.com/ohzqq/gbooks"
 	"github.com/spf13/cobra"
 )
 
 func searchCmdRun(cmd *cobra.Command, args []string) {
-	println(cmd.Name())
+	req := gbooks.NewRequest()
+	println(req.String())
 }
---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ohzqq/gbooks/cmd/cmd"
+
+func main() {
+	cmd.Execute()
+}
